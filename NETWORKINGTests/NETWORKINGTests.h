f5512b02818fdaa39f4ef4b2381df8a9680bdca5
Arihant Jain <Arihant@Arihants-MacBook-Pro.local>
2012-08-07 11:32:47 -0400

    Initial Commit

---
+++ NETWORKINGTests/NETWORKINGTests.h
@@ -0,0 +1,13 @@
+//
+//  NETWORKINGTests.h
+//  NETWORKINGTests
+//
+//  Created by Arihant Jain on 8/7/12.
+//  Copyright (c) 2012 DMA. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NETWORKINGTests : SenTestCase
+
+@end
---
+++ NETWORKINGTests/NETWORKINGTests.m
@@ -0,0 +1,32 @@
+//
+//  NETWORKINGTests.m
+//  NETWORKINGTests
+//
+//  Created by Arihant Jain on 8/7/12.
+//  Copyright (c) 2012 DMA. All rights reserved.
+//
+
+#import "NETWORKINGTests.h"
+
+@implementation NETWORKINGTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NETWORKINGTests");
+}
+
+@end
